2825985849b6bf72aad954b68f740ebf385b0932
George Devedlaka <gdevpri123@gmail.com>
2016-11-21 15:52:35 +0000

    Added model associations (customer has many may cause error)

--- app/models/business.rb
+++ app/models/business.rb
@@ -3,4 +3,8 @@ class Business < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :customers, through: :bookings
+  has_many :bookings
+  has_many :services
 end
