bf7d53af577349f4eeda37866e158e660a14ffe6
George Devedlaka <gdevpri123@gmail.com>
2016-11-23 12:20:45 +0000

    new homepage with correct navbar links

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   devise_for :businesses
   devise_for :customers
-  resources :bookings
   resources :businesses, except: :destroy do
-    resources :bookings
+    resources :bookings, except: [:new, :edit, :show]
   end
   root to: 'pages#home'
 end
