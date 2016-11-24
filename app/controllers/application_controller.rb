ed970d6db8844a9abcb47022922119ba72feb8a6
George Devedlaka <gdevpri123@gmail.com>
2016-11-24 11:01:36 +0000

    fixed strong params devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :address
-    devise_parameter_sanitizer.for(:sign_up) << :name
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:address, :name])
   end
 end
