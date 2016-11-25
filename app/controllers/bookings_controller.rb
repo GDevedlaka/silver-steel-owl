374c07ff1e072d35671a7e6ba519c4675c743636
Matthew Noble <matt_noble@hotmail.com>
2016-11-25 11:55:48 +0000

    fixed edit form issue

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,35 +1,30 @@
 class BookingsController < ApplicationController
-
-def index
-  @bookings = Booking.all
-
-end
-
-def show
-  @booking = Booking.find(params[:id])
-end
-
-def create
-  @booking = Booking.new(booking_params[:booking])
-  @business = Business.find(params[:business_id])
-  @booking.business = @business
-  if @booking.save
-    redirect_to dashboard_path
-  else
-    redirect_to business_path(@business)
-    flash[:alert]
+  def index
+    @bookings = Booking.all
   end
-end
 
+  def show
+    @booking = Booking.find(params[:id])
+  end
 
-private
+  def create
+    @booking = Booking.new(booking_params[:booking])
+    @business = Business.find(params[:business_id])
+    @booking.business = @business
+    binding.pry
+    if @booking.save
+      redirect_to dashboard_path
+    else
+      redirect_to business_path(@business)
+      flash[:alert]
+    end
+  end
 
-def booking_params
-  params.require(:booking).permit(:email, :name, :phone, :service_id)
-end
+  private
 
-
-
+  def booking_params
+    params.require(:booking).permit(:email, :name, :phone, :service_id)
+  end
 end
 
 # DateTime.parse(params[:date])
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,4 @@
 class Booking < ApplicationRecord
-  belongs_to :customer
   belongs_to :service
   belongs_to :business
 end
