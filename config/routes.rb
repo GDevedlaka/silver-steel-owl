c703631277b10f3d03eb316f50839d8fe5199fe0
George Devedlaka <gdevpri123@gmail.com>
2016-11-28 15:57:46 +0000

    better edit page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :businesses
   resources :businesses, except: :destroy do
     resources :bookings, except: [:new, :edit, :show]
-    resources :services, only: [:create]
+    resources :services, only: [:create, :destroy]
   end
   root to: 'pages#home'
   #business_root to: ''
