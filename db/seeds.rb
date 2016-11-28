d60f0b75ccd59ab2579db74ebac14f5e0f530050
George Devedlaka <gdevpri123@gmail.com>
2016-11-28 13:43:10 +0000

    finish business show first version

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,11 @@ Service.destroy_all
 Booking.destroy_all
 
 davesbarber = Business.create!(address: "22 Elder Street, London", name: "Dave's Barber", phone_number: "020 8343 2343", email: "dave@gmail.com", password: "dave123", description: "Most professional barber in Paddington")
-jimsdentist = Business.create!(address: "20 Folgate Street, London", name: "Jim's Dentist", phone_number: "020 2343 6763", email: "jim@gmail.com", password: "jim123", description: "Most professional dentist in Folgate Street")
+jimsdentist = Business.create!(address: "20 Folgate Street, London", name: "Jim's Dentist", phone_number: "020 2343 6763", email: "jim@gmail.com", password: "jim123", description: "At our Practice in Shoreditch, London, we provide both Private and NHS care for adults and children at Dentessentials.
+
+This website gives more information about Dentessentials and the services we provide. If the answer to your question is not included here, please contact us for further information.
+
+We offer a full range of routine and cosmetic dentistry. Treatments include silver amalgam or composite (white) fillings, root fillings, crowns, bridges and dentures. All routine examinations include radiographs, oral hygeine prevention advice and written treatment plan with costing.")
 judysnails = Business.create!(address: "10 Baker Street, London", name: "Judy's Nails", phone_number: "020 893 4479", email: "judy@gmail.com", password: "judy123", description: "Most professional nails specialist in Baker Street")
 
 checkup = Service.create!(name: "full checkup", price: 200, duration: 30, business: jimsdentist)
