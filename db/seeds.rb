16495491f648257a485292d3a660e1d0e6bff374
George Devedlaka <gdevpri123@gmail.com>
2016-11-29 10:13:04 +0000

    Removed yellow background

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,9 +22,10 @@ checkup = Service.create!(name: "full checkup", price: 200, duration: 30, busine
 filling = Service.create!(name: "filling", price: 100, duration: 15, business: jimsdentist)
 tooth = Service.create!(name: "tooth removal", price: 300, duration: 60, business: jimsdentist)
 haircut = Service.create!(name: "Haircut", price: 70, duration: 60, business: davesbarber)
+consulation = Service.create!(name: "consulation", price: 70, duration: 180, business: jimsdentist)
 
-
-Booking.create!(email: "alex@me.com", booking_time: Time.now, price: 200, status: "pending", service: checkup, business: jimsdentist)
-Booking.create!(email: "alex@me.com", booking_time: Time.now, price: 200, status: "approved", service: checkup, business: jimsdentist)
+Booking.create!(email: "alex@me.com", booking_time: Time.now + 300, price: 200, status: "pending", service: checkup, business: jimsdentist)
+Booking.create!(email: "alex@me.com", booking_time: Time.now + 400, price: 200, status: "approved", service: checkup, business: jimsdentist)
 Booking.create!(email: "matt@me.com", booking_time: Time.now, price: 300, status: "pending", service: tooth, business: jimsdentist)
-Booking.create!(email: "george@me.com", booking_time: Time.now, price: 30, status: "completed", service: haircut, business: davesbarber)
+Booking.create!(email: "george@me.com", booking_time: Time.now + 1000, price: 30, status: "completed", service: haircut, business: davesbarber)
+Booking.create!(email: "george@me.com", booking_time: Time.now + 2000, price: 30, status: "completed", service: consulation, business: jimsdentist)
